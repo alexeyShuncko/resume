95a3d1d9c29698a8119ae342383de76e17ee23ac
alexeyShuncko <shuncko.a@yandex.ru>
2022-10-13 17:33:06 +0300

    Русская версия

--- src/components/dataRus.js
+++ src/components/dataRus.js
@@ -12,52 +12,49 @@ import RestaurantIcon from '@mui/icons-material/Restaurant';
 
 
 
-
-
-
-
 export const dataRus = {
-    profileText: {name: 'Aleksei Shunko',job: 'Junior Frontend Developer'},
+    profileText: {name: 'Алексей Шунько',job: 'Junior Frontend Developer'},
     contactInfo: {
-      title: 'Contact Info', 
+      title: 'Контакты', 
       list: [
         {icon: <PhoneIcon />, text: '+375 33 335 23 28'},
         {icon: <TelegramIcon />, text: '@alexeyShuncko'},
         {icon: <GitHubIcon />, text: 'github.com/alexeyShuncko'},
         {icon: <MailOutlineIcon />, text: 'shuncko.a@yandex.ru'},
-        {icon: <LocationOnIcon />, text: 'Belarus, Minsk'},
+        {icon: <LocationOnIcon />, text: 'Беларусь, Минск'},
       ]},
-      education: {title: 'Education', list: [
-        {date: '2021-present', 
-        speciality: 'Self-education, learning programming. Online course of 100 lessons on React, Redux.', 
+      education: {title: 'Образование', list: [
+        {date: '2021-настоящее время', 
+        speciality: 'Самообразование, изучение программирования. Онлайн Курс из 100 уроков по React, Redux', 
         university: 'Youtube, lern.javascript, developer.mozilla etc.'},
         {date: '2010-2015', 
-        speciality: 'Design, Construction and Exploitation of Gas and Oil Pipelines and Storages', 
-        university: 'Polotsk State University'},
+        speciality: 'Проектирование, сооружение и эксплуатация газо-нефтепроводов и газо-нефтехранилищ', 
+        university: 'Полоцкий Государственный Университет'},
     ]},
-    languages: {title: 'Languages', list: [
-      {name: 'Russian', percent: '95%'},
-      {name: 'English', percent: '35%'},
-      {name: 'Bellarusian', percent: '95%'},
+    languages: {title: 'Языки', list: [
+      {name: 'Русский', percent: '95%'},
+      {name: 'Английский', percent: '35%'},
+      {name: 'Беларусский', percent: '95%'},
     ]},
-    about: {title: 'Profile', 
-    p1: `I'm a cheerful guy 29 years old who likes programming, outdoor activities, team sports.
-    Every day I try to learn something new. If you ask me: “What is the name of your favorite football team?”,
-     then I will answer - “Arsenal”.`},
-    experience: {title: 'Experience', list: [
-      {date: '2021-present', name: 'GitHub, freelance', job: 'Junior Frontend Developer', 
-      description: `Practical application of acquired knowledge. Development of own projects on github,
-       each of which is deployed on github pages. Execution of test tasks. Freelance.`},
-      {date: '2015-2022', name: 'Gazprom transgaz Belarus,', job: 'Dispatcher', 
-      description: `Work in various positions, the last of which is the dispatcher. Responsibilities:
-      operational management of gas transportation through the gas transmission system of the branch,
-      constant monitoring of the parameters of the gas transmission system of the branch,
-      study and application in work of new technologies and normative and technical documents,
-      daily responsibility for subordinate personnel and the work they do,  
-      accounting for the amount of gas supplied to consumers with
-      constant interaction with gas supply organizations of the country.`},
+    about: {title: 'Обо мне', 
+    p1: `Жизнерадостный парень 29 лет, которому нравится программирование, активный отдых на природе, 
+    командные спортивные игры. Каждый день стараюсь узнавать что-то новое. 
+    Если меня спросить: "Как называется твоя любимая футбольная команда?", то я отвечу – "Арсенал".`},
+    experience: {title: 'Опыт', list: [
+      {date: '2021-настоящее время', name: 'GitHub, фриланс', job: 'Junior Frontend Developer', 
+      description: `Практическое применение получаемых знаний. 
+      Разработка собственных проектов на GitHub, каждый из которых развёрнут на GitHub Pages. 
+      Выполнение тестовых заданий. Фриланс.`},
+      {date: '2015-2022', name: 'Газпром трансгаз Беларусь,', job: 'Диспетчер', 
+      description: `Работа на различных должностях, последняя из которых диспетчер. Обязанности: 
+      оперативное руководство транспортировкой газа по газотранспортной системе филиала,
+      постоянный контроль за параметрами газотранспортной системы филиала,
+      изучение и применение в работе новых технологий и нормативно-технических документов,
+      ежедневное несение ответственности за подчиненный персонал и выполняемую ими работу,
+      учет количества газа, поставленного потребителям с постоянным взаимодействием с газоснабжающими 
+      организациями страны.`},
     ]},
-    skills: {title: 'Professional Skills', list: [
+    skills: {title: 'Профессиональные навыки', list: [
       {name: 'HTML', percent: '90%'},
       {name: 'CSS, SASS', percent: '85%'},
       {name: 'Javascript', percent: '90%'},
@@ -68,10 +65,10 @@ export const dataRus = {
       {name: 'Webpack', percent: '70%'},
       {name: 'AntDesign, MUI', percent: '85%'},
     ]},
-    interest: {title: 'Interest', list: [
-      {icon: <SportsEsportsIcon fontSize='large'/>, name: 'Gaming'},
-      {icon: <SportsSoccerIcon fontSize='large'/>, name: 'Football'},
-      {icon: <RestaurantIcon fontSize='large'/>, name: 'Cooking'},
-      {icon: <CodeIcon fontSize='large'/>, name: 'Programming'},
+    interest: {title: 'Интересы', list: [
+      {icon: <SportsEsportsIcon fontSize='large'/>, name: 'Игры'},
+      {icon: <SportsSoccerIcon fontSize='large'/>, name: 'Футбол'},
+      {icon: <RestaurantIcon fontSize='large'/>, name: 'Готовка'},
+      {icon: <CodeIcon fontSize='large'/>, name: 'Фронтенд'},
     ]}
   }
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,144 +1,145 @@
 import { useState } from "react";
-import { dataEng} from "./dataEng";
-import { dataRus} from "./dataRus";
+import { dataEng } from "./dataEng";
+import { dataRus } from "./dataRus";
 import photo from '../images/photo1.jpg'
 
 
 
 
 
-const Resume =(props)=> {
+const Resume = (props) => {
 
-    const [language, setLanguage] = useState(false)
-    
-    const data = !language ? dataEng : dataRus
+  const [language, setLanguage] = useState(false)
+
+  const data = !language ? dataEng : dataRus
 
 
-    const handlerLanguage =()=> {
-        setLanguage(!language)
-    }
-  
+  const handlerLanguage = () => {
+    setLanguage(!language)
+  }
+
   return (
     <div className='container'>
 
-    <div className='leftSection'>
+      <div className='blockBtn'>
+        {
+          !language
+            ? <button onClick={handlerLanguage}>RUS</button>
+            : <button onClick={handlerLanguage}>ENG</button>
+        }
 
-        <div className='blockBtn'>
-            {
-               !language
-               ? <button onClick={handlerLanguage}>RU</button>
-               : <button onClick={handlerLanguage}>ENG</button>
+        <button>Night</button>
+      </div>
+
+      <div className='leftSection'>
+
+        <div className='profileText'>
+          <div className='imgBx'>
+            <img src={photo} alt='My_photo' />
+          </div>
+          <h2>{data.profileText.name}<br /><span>{data.profileText.job}</span></h2>
+        </div>
+
+        <div className='contactInfo'>
+          <h3 className='title'>{data.contactInfo.title}</h3>
+          <ul>
+            {data.contactInfo.list.map(el => (
+              <li key={el.text}>
+                <span className='icon'>{el.icon}</span>
+                <span className='text'>{el.text}</span>
+              </li>
+            ))
             }
-       
-        <button>RUS</button>
+          </ul>
         </div>
 
-
-      <div className='profileText'>
-        <div className='imgBx'>
-          <img src={photo} alt='My_photo' />
-        </div>
-        <h2>{data.profileText.name}<br /><span>{data.profileText.job}</span></h2>
-      </div>
-
-      <div className='contactInfo'>
-        <h3 className='title'>{data.contactInfo.title}</h3>
-        <ul>
-          {data.contactInfo.list.map(el => (
-            <li key={el.text}>
-              <span className='icon'>{el.icon}</span>
-              <span className='text'>{el.text}</span>
-            </li>
-          ))
-          }
-        </ul>
-      </div>
-
-      <div className='contactInfo education'>
-        <h3 className='title'>{data.education.title}</h3>
-        <ul>
-          {data.education.list.map((el, i) => (
-            <li key={i}>
-              <h5>{el.date}</h5>
-              <h4>{el.speciality}</h4>
-              <h4>{el.university}</h4>
-            </li>
-          ))
-          }
-        </ul>
-      </div>
-
-      <div className='contactInfo language'>
-        <h3 className='title'>{data.languages.title}</h3>
-        <ul>
-          {
-            data.languages.list.map(el => (
-              <li key={el.name}>
-                <span className='text'>{el.name}</span>
-                <span className='percent'>
-                  <div style={{ width: el.percent }}></div>
-                </span>
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-    </div>
-
-    <div className='rightSection'>
-      <div className='about'>
-        <h2 className='title2'>{data.about.title}</h2>
-        <p>{data.about.p1}</p>
-      </div>
-
-      <div className='about'>
-        <h2 className='title2'>{data.experience.title}</h2>
-        {
-          data.experience.list.map(el => (
-            <div className='box' key={el.name}>
-              <div className='years_company'>
+        <div className='contactInfo education'>
+          <h3 className='title'>{data.education.title}</h3>
+          <ul>
+            {data.education.list.map((el, i) => (
+              <li key={i}>
                 <h5>{el.date}</h5>
-                <h5>{el.name}</h5>
-              </div>
-              <div className='text'>
-                <h4>{el.job}</h4>
-                <p>{el.description}</p>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-
-      <div className='about skills'>
-        <h2 className='title2'>{data.skills.title}</h2>
-        {
-          data.skills.list.map(el => (
-            <div className='box' key={el.name}>
-              <h4>{el.name}</h4>
-              <div className='percent'>
-                <div style={{ width: el.percent }}></div>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-
-      <div className='about interest'>
-        <h2 className='title2'>{data.interest.title}</h2>
-        <ul>
-          {
-            data.interest.list.map(el => (
-              <li key={el.name}>
-                <span>{el.icon}</span>
-                {el.name}
+                <h4>{el.speciality}</h4>
+                <h4>{el.university}</h4>
               </li>
             ))
-          }
-        </ul>
-      </div>
-    </div>
+            }
+          </ul>
+        </div>
 
-  </div>
-)
+        <div className='contactInfo language'>
+          <h3 className='title'>{data.languages.title}</h3>
+          <ul>
+            {
+              data.languages.list.map(el => (
+                <li key={el.name}>
+                  <span className='text'>{el.name}</span>
+                  <span className='percent'>
+                    <div style={{ width: el.percent }}></div>
+                  </span>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+
+      <div className='rightSection'>
+
+
+        <div className='about'>
+          <h2 className='title2'>{data.about.title}</h2>
+          <p>{data.about.p1}</p>
+        </div>
+
+        <div className='about'>
+          <h2 className='title2'>{data.experience.title}</h2>
+          {
+            data.experience.list.map(el => (
+              <div className='box' key={el.name}>
+                <div className='years_company'>
+                  <h5>{el.date}</h5>
+                  <h5>{el.name}</h5>
+                </div>
+                <div className='text'>
+                  <h4>{el.job}</h4>
+                  <p>{el.description}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='about skills'>
+          <h2 className='title2'>{data.skills.title}</h2>
+          {
+            data.skills.list.map(el => (
+              <div className='box' key={el.name}>
+                <h4>{el.name}</h4>
+                <div className='percent'>
+                  <div style={{ width: el.percent }}></div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='about interest'>
+          <h2 className='title2'>{data.interest.title}</h2>
+          <ul>
+            {
+              data.interest.list.map(el => (
+                <li key={el.name}>
+                  <span>{el.icon}</span>
+                  {el.name}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 export default Resume
